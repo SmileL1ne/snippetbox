517b8e0da4828482add7a1901f8b350f8aa8ea23
Sarvarov Mustafa <sarvarov.mustafa@gmail.com>
2023-12-25 17:02:26 +0600

    refactor: structure code, feat: add logger, add options through flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,43 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
+	cfg := &config{}
+	flag.StringVar(&cfg.addr, "addr", ":7000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static", "./ui/static", "Directory path for static files")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	app := &application{
+		logger: logger,
+	}
+
 	mux := http.NewServeMux()
 
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./ui/static"))))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
-	log.Print("Starting server on :7000")
-	err := http.ListenAndServe(":7000", mux)
-	log.Fatal(err)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+	logger.Info("starting the server", slog.String("addr", cfg.addr))
+	err := http.ListenAndServe("127.0.0.1"+cfg.addr, mux)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
