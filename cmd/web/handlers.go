65dfb43b0f4144b83cc50b05ad00a2a2b13f1fd7
Sarvarov Mustafa <sarvarov.mustafa@gmail.com>
2023-12-27 23:58:50 +0600

    feat: dynamic data display, use funcs in templates, style: prettier dates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -21,9 +21,8 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data := templateData{
-		Snippets: snippets,
-	}
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
 
 	app.render(w, r, http.StatusOK, "home.html", data)
 }
@@ -44,9 +43,8 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, r, err)
 	}
 
-	data := templateData{
-		Snippet: snippet,
-	}
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
 
 	app.render(w, r, http.StatusOK, "view.html", data)
 }
