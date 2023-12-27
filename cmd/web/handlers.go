18b7c437d5e0747f7b6e8da602154280a6d626f0
Sarvarov Mustafa <sarvarov.mustafa@gmail.com>
2023-12-28 01:45:18 +0600

    fix: delete deliberate panic

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -15,8 +15,6 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	panic("nah")
-
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
