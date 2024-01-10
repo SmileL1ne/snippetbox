f96c7b56bd1f54b3609a5f1d34a0b5aee33cffc4
Sarvarov Mustafa <sarvarov.mustafa@gmail.com>
2024-01-11 02:15:27 +0600

    feat: add authentication with context, add embedded filesystem

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
+	"snippetbox.msarvaro.com/ui"
 )
 
 func (app *application) routes() http.Handler {
@@ -17,10 +18,10 @@ func (app *application) routes() http.Handler {
 		app.methodNotAllowed(w)
 	})
 
-	fileServer := http.FileServer(http.Dir(app.cfg.staticDir))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static/", fileServer))
+	fileServer := http.FileServer(http.FS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*filepath", fileServer)
 
-	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
@@ -36,6 +37,5 @@ func (app *application) routes() http.Handler {
 	router.Handler(http.MethodPost, "/snippet/create", protected.ThenFunc(app.snippetCreatePost))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-
 	return standard.Then(router)
 }
